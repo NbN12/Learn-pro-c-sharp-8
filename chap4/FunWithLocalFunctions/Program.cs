6d49c91a760b45cbf40986cab91072218199ce32
Jack Wonder <babyfacedeveloper@gmail.com>
2020-12-16 22:56:10 +0700

    Add proejct fun with local functions

---
+++ chap4/FunWithLocalFunctions/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace FunWithLocalFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static int AddWrapperWithStatic(int x, int y)
+        {
+            //Do some validation here
+            return Add(x, y);
+
+            static int Add(int x, int y)
+            {
+                x += 1;
+                return x + y;
+            }
+        }
+
+        static int AddWrapperWithSideEffect(int x, int y)
+        {
+            //Do some validation here
+            return Add();
+
+            int Add()
+            {
+                x += 1;
+                return x + y;
+            }
+        }
+
+        static int Add(int x, int y)
+        {
+            //Do some validation here
+            return x + y;
+        }
+
+        static int AddWrapper(int x, int y)
+        {
+            return Add();
+            int Add()
+            {
+                return x + y;
+            }
+        }
+    }
+}
