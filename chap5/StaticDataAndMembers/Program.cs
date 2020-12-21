32d11cedc3c17115cc771154b2febb1dee535c11
Jack Wonder <babyfacedeveloper@gmail.com>
2020-12-21 08:08:16 +0700

    Update project using properties

--- chap5/StaticDataAndMembers/Program.cs
+++ chap5/StaticDataAndMembers/Program.cs
@@ -10,20 +10,24 @@ namespace StaticDataAndMembers
             // Make an account.
             SavingsAccount s1 = new SavingsAccount(50);
             // Print the current interest rate.
-            Console.WriteLine("Interest Rate is: {0}", SavingsAccount.GetInterestRate());
+            // Console.WriteLine("Interest Rate is: {0}", SavingsAccount.GetInterestRate());
+            Console.WriteLine("Interest Rate is: {0}", SavingsAccount.InterestRate);
 
             // Try to change the interest rate via property.
-            SavingsAccount.SetInterestRate(0.08);
+            // SavingsAccount.SetInterestRate(0.08);
+            SavingsAccount.InterestRate = 0.08;
 
             // Make a second account.
             SavingsAccount s2 = new SavingsAccount(100);
 
             // Should print 0.08...right??
-            Console.WriteLine("Interest Rate is: {0}", SavingsAccount.GetInterestRate());
+            // Console.WriteLine("Interest Rate is: {0}", SavingsAccount.GetInterestRate());
+            Console.WriteLine("Interest Rate is: {0}", SavingsAccount.InterestRate);
 
             // Make new object, this does NOT 'reset' the interest rate.
             SavingsAccount s3 = new SavingsAccount(10000.75);
-            Console.WriteLine("Interest Rate is: {0}", SavingsAccount.GetInterestRate());
+            // Console.WriteLine("Interest Rate is: {0}", SavingsAccount.GetInterestRate());
+            Console.WriteLine("Interest Rate is: {0}", SavingsAccount.InterestRate);
 
             // This is just fine.
             TimeUtilClass.PrintDate();
--- chap5/StaticDataAndMembers/SavingsAccount.cs
+++ chap5/StaticDataAndMembers/SavingsAccount.cs
@@ -2,13 +2,14 @@ using System;
 
 namespace StaticDataAndMembers
 {
+    // A simple savings account class.
     public class SavingsAccount
     {
         // Instance-level data
         public double currBalance;
 
         // A static point of data.
-        public static double currInterestRate;
+        private static double _currInterestRate;
 
         // Notice that our constructor is setting
         // the static currInterestRate value.
@@ -21,14 +22,15 @@ namespace StaticDataAndMembers
         static SavingsAccount()
         {
             Console.WriteLine("In static ctor!");
-            currInterestRate = 0.04;
+            _currInterestRate = 0.04;
         }
 
         // public SavingsAccount(double balance) => currBalance = balance;
-
-        public static double GetInterestRate() => currInterestRate;
-
+        // public static double GetInterestRate() => _currInterestRate;
         // Static members to get/set interest rate.
-        public static void SetInterestRate(double newRate) => currInterestRate = newRate;
+        // public static void SetInterestRate(double newRate) => _currInterestRate = newRate;
+
+        // A Static property.
+        public static double InterestRate { get => _currInterestRate; set => _currInterestRate = value; }
     }
 }
