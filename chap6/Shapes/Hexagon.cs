1e5a9c57da0b5ef82524a637efe1407f1034390a
Jack Wonder <babyfacedeveloper@gmail.com>
2020-12-23 23:21:48 +0700

    Add project Shapes

---
+++ chap6/Shapes/Hexagon.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Shapes
+{
+    class Hexagon : Shape
+    {
+        public Hexagon() { }
+        public Hexagon(string name) : base(name) { }
+
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing {0} the Hexagon", PetName);
+        }
+    }
+}
