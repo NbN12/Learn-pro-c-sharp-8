fb55e970ff2faebad8e0f1b722885d5ddf7441c4
Babyface <babyfacedeveloper@gmail.com>
2020-12-28 00:00:56 +0700

    Add project Custom Exception

---
+++ chap7/CustomException/CarIsDeadException.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Runtime.Serialization;
+
+namespace CustomException
+{
+    // This custom exception describes the details of the car-is-dead condition.
+    // (Remember, you can also simply extend Exception.)
+    [Serializable]
+    public class CarIsDeadException : ApplicationException
+    {
+        public DateTime ErrorTimeStamp { get; set; }
+        public string CauseOfError { get; set; }
+
+        public CarIsDeadException() { }
+        public CarIsDeadException(string cause, DateTime time)
+        : this(cause, time, string.Empty) { }
+        public CarIsDeadException(string cause, DateTime time, string message)
+        : this(cause, time, message, null) { }
+        public CarIsDeadException(string cause, DateTime time, string message, Exception inner) :
+        base(message, inner)
+        {
+            CauseOfError = cause;
+            ErrorTimeStamp = time;
+        }
+
+        protected CarIsDeadException(string cause, DateTime time, SerializationInfo info, StreamingContext context)
+        : base(info, context)
+        {
+            CauseOfError = cause;
+            ErrorTimeStamp = time;
+        }
+
+    }
+}
