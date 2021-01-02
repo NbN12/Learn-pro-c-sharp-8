845077890a9211239e98153721bdaf14e3094f47
Babyface <babyfacedeveloper@gmail.com>
2021-01-02 22:52:16 +0700

    Add project Custom Enumerator

---
+++ chap8/CustomEnumerator/Garage.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+
+namespace CustomEnumerator
+{
+    // Garage contains a set of Car objects.
+    public class Garage : IEnumerable
+    {
+        private Car[] carArray = new Car[4];
+
+        // Fill with some Car objects upon startup.
+        public Garage()
+        {
+            carArray[0] = new Car("Rusty", 30);
+            carArray[1] = new Car("Clunker", 55);
+            carArray[2] = new Car("Zippy", 30);
+            carArray[3] = new Car("Fred", 30);
+        }
+
+        // Return the array object's IEnumerator.
+        public IEnumerator GetEnumerator() => carArray.GetEnumerator();
+    }
+}
---
+++ chap8/CustomEnumerator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CustomEnumerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with IEnumerable / IEnumerator *****\n");
+            Garage carLot = new Garage();
+            // Hand over each car in the collection?
+            foreach (Car c in carLot)
+            {
+                Console.WriteLine("{0} is going {1} MPH", c.PetName, c.CurrentSpeed);
+            }
+            Console.ReadLine();
+        }
+    }
+}
