02701822f494d3a4e9b0f1cefe0b47a2eec6df0e
Babyface <babyfacedeveloper@gmail.com>
2021-01-11 21:28:38 +0700

    Add project Simple GC

---
+++ chap9/SimpleGC/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace SimpleGC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** GC Basics *****");
+            // Print out estimated number of bytes on heap.
+            Console.WriteLine("Estimated bytes on heap: {0}", GC.GetTotalMemory(false));
+            // MaxGeneration is zero based, so add 1 for display
+            // purposes.
+            Console.WriteLine("This OS has {0} object generations.\n", (GC.MaxGeneration + 1));
+
+            Car refToMyCar = new Car("Zippy", 100);
+            Console.WriteLine(refToMyCar.ToString());
+
+            // Print out generation of refToMyCar object.
+            Console.WriteLine("Generation of refToMyCar is: {0}\n", GC.GetGeneration(refToMyCar));
+
+            // Make a ton of objects for testing purposes.
+            object[] tonsOfObjects = new object[50000];
+            for (int i = 0; i < tonsOfObjects.Length; i++)
+            {
+                tonsOfObjects[i] = new object();
+            }
+
+            // Collect only gen 0 objects.
+            Console.WriteLine("Force Garbage Collection");
+            GC.Collect(0, GCCollectionMode.Forced);
+            GC.WaitForPendingFinalizers();
+
+            // Print out generation of refToMyCar.
+            Console.WriteLine("Generation of refToMyCar is: {0}", GC.GetGeneration(refToMyCar));
+
+            if (tonsOfObjects[9000] != null)
+            {
+                Console.WriteLine("Generation of tonsOfObjects[9000] is: {0}", GC.GetGeneration(tonsOfObjects[9000]));
+            }
+            else
+            {
+                Console.WriteLine("tonsOfObjects[9000] is no longer alive.");
+            }
+
+            // Print out how many times a generation has been swept.
+            Console.WriteLine("\nGen 0 has been swept {0} times", GC.CollectionCount(0));
+            Console.WriteLine("Gen 1 has been swept {0} times", GC.CollectionCount(1));
+            Console.WriteLine("Gen 2 has been swept {0} times", GC.CollectionCount(2));
+
+            Console.ReadLine();
+        }
+    }
+}
