12a2a47833368279da2621358cb87aed995e5309
Babyface <babyfacedeveloper@gmail.com>
2021-02-11 18:55:15 +0700

    Fun with linq expressions

---
+++ chap_13/FunWithLinqExpressions/ProductInfoSmall.cs
@@ -0,0 +1,9 @@
+namespace FunWithLinqExpressions
+{
+    class ProductInfoSmall
+    {
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+        public override string ToString() => $"Name={Name}, Description={Description}";
+    }
+}
