46e550935528e91d6519bbe9a5fc2cb00925b19d
Jack Wonder <babyfacedeveloper@gmail.com>
2021-05-15 16:48:55 +0700

    Create sln and ThreadStats proj

---
+++ chap_15/ThreadStats/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ThreadStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Primary Thread stats *****\n");
+
+            Thread primaryThread = Thread.CurrentThread;
+            primaryThread.Name = "ThePrimaryThread";
+
+            // Print out some stats about this thread.
+            Console.WriteLine($"ID of current thread: {primaryThread.ManagedThreadId}");
+            Console.WriteLine($"Thread Name: {primaryThread.Name}");
+            Console.WriteLine($"Has thread started?: {primaryThread.IsAlive}");
+            Console.WriteLine($"Priority Level: {primaryThread.Priority}");
+            Console.WriteLine($"Thread State: {primaryThread.ThreadState}");
+            Console.ReadLine();
+        }
+    }
+}
