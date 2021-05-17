545990a081a036f03fd62fb51e87e3a3a336935e
Jack Wonder <babyfacedeveloper@gmail.com>
2021-05-17 21:34:07 +0700

    create project fun with thread pool

---
+++ chap_15/FunWithThreadPool/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace FunWithThreadPool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with the CoreCLR Thread Pool *****\n");
+
+            Console.WriteLine($"Main thread started. ThreadID = {Thread.CurrentThread.ManagedThreadId}");
+
+            Printer p = new Printer();
+
+            WaitCallback workItem = new WaitCallback(PrintTheNumbers);
+
+            for (int i = 0; i < 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(workItem, p);
+            }
+            Console.WriteLine("All tasks queued");
+            Console.ReadLine();
+        }
+
+        static void PrintTheNumbers(object state)
+        {
+            Printer task = (Printer)state;
+            task.PrintNumbers();
+        }
+    }
+}
