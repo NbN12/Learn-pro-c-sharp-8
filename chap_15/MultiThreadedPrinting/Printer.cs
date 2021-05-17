5a364318eca38d33ad3119095afe246371459095
Jack Wonder <babyfacedeveloper@gmail.com>
2021-05-17 19:20:27 +0700

    using lock keyword

--- chap_15/MultiThreadedPrinting/Printer.cs
+++ chap_15/MultiThreadedPrinting/Printer.cs
@@ -5,20 +5,29 @@ namespace MultiThreadedPrinting
 {
     public class Printer
     {
+        // Lock token
+        private object threadLock = new object();
         public void PrintNumbers()
         {
-            // Display Thread info.
-            Console.WriteLine("-> {0} is executing PrintNumbers()",
-            Thread.CurrentThread.Name);
-
-            for (var i = 0; i < 10; i++)
+            // Use the private object lock token.
+            lock (threadLock)
             {
-                Random r = new Random();
-                Thread.Sleep(1000 * r.Next(5));
-                Console.Write($"{i}, ");
-            }
+                // Display Thread info.
+                Console.WriteLine("-> {0} is executing PrintNumbers()",
+                Thread.CurrentThread.Name);
 
-            Console.WriteLine();
+                // Print out numbers.
+                Console.Write("Your numbers: ");
+
+                for (var i = 0; i < 10; i++)
+                {
+                    Random r = new Random();
+                    Thread.Sleep(1000 * r.Next(5));
+                    Console.Write($"{i}, ");
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
