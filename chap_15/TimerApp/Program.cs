3d007e174cafa9ed92fd31f792bf02a9ea28d6e3
Jack Wonder <babyfacedeveloper@gmail.com>
2021-05-17 21:20:15 +0700

    create project timer

---
+++ chap_15/TimerApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace TimerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Working with Timer type *****\n");
+
+            // Create the delegate for the Timer type.
+            TimerCallback timeCB = new TimerCallback(PrintTime);
+
+            Timer t = new Timer(
+                timeCB, // The TimerCallback delegate object.
+                "Hello From Main", // Any info to pass into the called method (null for no info).
+                0, // Amount of time to wait before starting (in milliseconds).
+                1000 // Interval of time between calls (in milliseconds).
+            );
+
+            Console.WriteLine("Hit Enter key to terminate...");
+            Console.ReadLine();
+        }
+
+        static void PrintTime(object state)
+        {
+            Console.WriteLine($"Time is: {DateTime.Now.ToLongTimeString()}, Param is: {state.ToString()}");
+        }
+    }
+}
