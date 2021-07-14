703bc0ac9f8003a30afadb28690e2702981e676e
Baby face dev <babyfacedeveloper@gmail.com>
2021-07-14 22:35:42 +0700

    create proj late binding with dynamic

---
+++ chap_18/LateBindingWithDynamic/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.CSharp.RuntimeBinder;
+
+namespace LateBindingWithDynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddWithReflection();
+        }
+
+        private static void AddWithReflection()
+        {
+            var asm = Assembly.LoadFrom($"{AppDomain.CurrentDomain.BaseDirectory}MathLibrary");
+            try
+            {
+                // Get metadata for the SimpleMath type.
+                var math = asm.GetType("MathLibrary.SimpleMath");
+
+                // // Create a SimpleMath on the fly.
+                // var obj = Activator.CreateInstance(math);
+
+                // // Get info for Add.
+                // var mi = math.GetMethod("Add");
+
+                // // Invoke method (with parameters).
+                // object[] args = { 10, 70 };
+
+                // Console.WriteLine($"Result is: {mi.Invoke(mi, args)}");
+
+                // using dynamic
+
+                // Create a SimpleMath on the fly.
+                dynamic obj = Activator.CreateInstance(math);
+
+                // Note how easily we can now call Add().
+                Console.WriteLine($"Result is: {obj.Add(10, 70)}");
+            }
+            // catch (Exception ex)
+            // {
+            //     Console.WriteLine(ex.Message);
+            // }
+            catch (RuntimeBinderException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
